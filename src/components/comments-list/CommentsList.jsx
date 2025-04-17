c7b1b543c32b735b23774983ca0f5c326af9545b
Vlad Bas <70838774+fogirtamo@users.noreply.github.com>
2025-04-17 18:53:46 +0300

    change: Comments List

--- src/components/comments-list/CommentsList.jsx
+++ src/components/comments-list/CommentsList.jsx
@@ -4,31 +4,28 @@ import { useSelector } from 'react-redux'
 import { selectComments } from 'features/userSlice'
 
 const CommentsList = () => {
-    const commentsList = useSelector(selectComments);
+  const commentsList = useSelector(selectComments) || []; // Добавлена проверка на undefined
 
-    return (
-        commentsList.length > 0 ?
-            <div className={classes.commentsList}>
-                {
-                    commentsList.map((comment) => {
-                        return (
-                            <div
-                                className={classes.comment}
-                                key={`${comment.date}`}
-                            >
-                                <div className={classes.commentInfo}>
-                                    <div className={classes.commentInfo_nickname}>{comment.nickname}</div>
-                                    <div className={classes.commentInfo_date}>{comment.formattedDate}</div>
-                                </div>
-                                <div className={classes.commentText}>{comment.comment}</div>
-                            </div>
-                        )
-                    })
-                }
+  return (
+    commentsList.length > 0 ? (
+      <div className={classes.commentsList}>
+        {commentsList.map((comment) => (
+          <div
+            className={classes.comment}
+            key={`${comment.date}`}
+          >
+            <div className={classes.commentInfo}>
+              <div className={classes.commentInfo_nickname}>{comment.nickname}</div>
+              <div className={classes.commentInfo_date}>{comment.formattedDate}</div>
             </div>
-            :
-            <></>
+            <div className={classes.commentText}>{comment.comment}</div>
+          </div>
+        ))}
+      </div>
+    ) : (
+      <></>
     )
-}
+  );
+};
 
-export default CommentsList
+export default CommentsList;
